6b3a75326f81336e9a3156fc452ae123b637f656
suleymanharbelioglu <harba.suleyman@gmail.com>
2025-09-23 12:15:59 +0300

    favorite icon button

--- lib/presentation/product_detail/pages/product_detail.dart
+++ lib/presentation/product_detail/pages/product_detail.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/common/widgets/appbar/app_bar.dart';
 import 'package:flutter_ecommerce_app/domain/product/entity/product.dart';
+import 'package:flutter_ecommerce_app/presentation/product_detail/bloc/favorite_icon_cubit.dart';
+import 'package:flutter_ecommerce_app/presentation/product_detail/widgets/favorite_button.dart';
 
 class ProductDetailPage extends StatelessWidget {
   final ProductEntity productEntity;
@@ -8,6 +11,25 @@ class ProductDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: BasicAppbar(hideBack: false));
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FavoriteIconCubit()..isFavorite(productEntity.productId),
+        ),
+      ],
+      child: Scaffold(
+        appBar: BasicAppbar(
+          hideBack: false,
+          action: FavoriteButton(productEntity: productEntity),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/product_detail/widgets/favorite_button.dart
+++ lib/presentation/product_detail/widgets/favorite_button.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/core/configs/theme/app_colors.dart';
+import 'package:flutter_ecommerce_app/domain/product/entity/product.dart';
+import 'package:flutter_ecommerce_app/presentation/product_detail/bloc/favorite_icon_cubit.dart';
 
 class FavoriteButton extends StatelessWidget {
-  const FavoriteButton({super.key});
+  final ProductEntity productEntity;
+  const FavoriteButton({super.key, required this.productEntity});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return IconButton(
+      onPressed: () {
+        context.read<FavoriteIconCubit>().onTap(productEntity);
+      },
+      icon: Container(
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          color: AppColors.secondBackground,
+          shape: BoxShape.circle,
+        ),
+        child: BlocBuilder<FavoriteIconCubit, bool>(
+          builder: (context, state) {
+            return Icon(
+              state ? Icons.favorite : Icons.favorite_outline,
+              size: 15,
+              color: Colors.white,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
